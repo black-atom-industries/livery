b282e5c662f204cd8e2dd8b3a8d664e447dd4c5e
nbr <nikolaus.brunner@protonmail.ch>
2026-03-16 20:25:40 +0100

    feat: register config commands and update FS capabilities [DEV-299]

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,30 @@
+mod config;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_fs::init())
-    .plugin(tauri_plugin_shell::init())
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![
+            config::get_config,
+            config::save_config,
+        ])
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+
+            // Scope FS paths from config on startup
+            let handle = app.handle().clone();
+            let cfg = config::read_config_from_disk();
+            config::scope_config_paths(&handle, &cfg);
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
